dcb29b42925a423cc7ba4d8bc38712c9a7e3359d
israelisaque <israelisaque@hotmail.com>
2021-10-05 01:48:15 -0300

    Obtendo informações da cidade e clima através da API AccuWeather

---
+++ js/app.js
@@ -0,0 +1,39 @@
+const APIKey = 'YBhR5fNZvhY3gX3eYVxNRWsH4t0oKkVd';
+
+const getCityUrl = cityName => `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${APIKey}&q=${cityName}`;
+
+const getCityData = async cityName => {
+  try {
+    const cityUrl = getCityUrl(cityName);
+    const response = await fetch(cityUrl);
+
+    if (!response.ok) {
+      throw new Error('Não foi possível obter os dados');
+    }
+
+    const [cityData] = await response.json();
+    return cityData;
+  } catch ({ name, message }) {
+    alert(`${name}: ${message}`);
+  }
+}
+
+const getCityWeather = async cityName => {
+  try {
+    const { Key } = await getCityData(cityName);
+    const cityWeatherUrl = `http://dataservice.accuweather.com/currentconditions/v1/${Key}?apikey=${APIKey}`;
+    const response = await fetch(cityWeatherUrl);
+
+    if (!response.ok) {
+      throw new Error('Não foi possível obter os dados');
+    }
+
+    const [cityWeatherData] = await response.json();
+    debugger
+    return cityWeatherData;
+  } catch ({ name, message }) {
+    alert(`${name}: ${message}`);
+  }
+}
+
+getCityWeather('recife');
